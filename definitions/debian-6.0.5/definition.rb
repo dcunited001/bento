f4ea84fb2a64a0768a5e728a476e5406b3f6bfd9
David Conner <dconner.pro@gmail.com>
2013-02-18 00:44:51 -0500

    updated the 6.0.5 basebox to work with the Debian 6.0.6
    basically just changed the iso name and md5 hash.
    tested and ran exported a basebox

--- definitions/debian-6.0.5/definition.rb
+++ definitions/debian-6.0.5/definition.rb
@@ -1,10 +1,12 @@
 require File.dirname(__FILE__) + "/../.debian/session.rb"
 
-iso = "debian-6.0.5-amd64-CD-1.iso"
+#iso = "debian-6.0.5-amd64-CD-1.iso"
+iso = "debian-6.0.6-amd64-CD-1.iso"
 
 session =
   DEBIAN_SESSION.merge( :iso_file => iso,
-                        :iso_md5 => "bccf90bdaf352a554e8af99f0af6c655",
-                        :iso_src => "http://cdimage.debian.org/debian-cd/6.0.5/amd64/iso-cd/#{iso}")
+                        #:iso_md5 => "bccf90bdaf352a554e8af99f0af6c655", #6.0.5
+                        :iso_md5 => "b81e837a5bc946c7af380c057afd2b46", #6.0.6
+                        :iso_src => "http://cdimage.debian.org/debian-cd/6.0.6/amd64/iso-cd/#{iso}")
 
 Veewee::Session.declare session
